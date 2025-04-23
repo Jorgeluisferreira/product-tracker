9b0d56f3d8dd9f07d68f545615bd22a0466e8a28
Jorge Luis <jorgejlfa@hotmail.com>
2025-04-23 14:17:54 -0300

    Remover arquivos de cache do repositório

--- factory/scraper_factory.py
+++
@@ -1,9 +0,0 @@
-from scrapers.relva import RelvaScraper
-
-class ScraperFactory:
-    @staticmethod
-    def get_scraper(url: str):
-        if "relvaco.com" in url:
-            return RelvaScraper()
-        else:
-            raise ValueError("Loja não suportada")
--- scrapers/base.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class ScraperStrategy(ABC):
-    @abstractmethod
-    def extrair_info(self, url: str) -> dict:
-        pass
