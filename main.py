acb163b32096b9fee16452cd0ad1a98246b8be20
Jorge Luis <jorgejlfa@hotmail.com>
2025-04-23 15:28:59 -0300

    Adiciona verificação de disponibilidade na relva

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from factory.scraper_factory import ScraperFactory
+import asyncio
 
 urls = [
     "https://relvaco.com.br/produto/camiseta-oversized-inferno-basica",
 ]
 
-for url in urls:
-    try:
-        scraper = ScraperFactory.get_scraper(url)
-        info = scraper.extrair_info(url)
-        print(info)
-    except Exception as e:
-        print(f"Erro ao processar {url}: {e}")
+async def main():
+    for url in urls:
+        try:
+            scraper = ScraperFactory.get_scraper(url)
+            info = await scraper.extrair_info(url)
+            print(info)
+        except Exception as e:
+            print(f"Erro ao processar {url}: {e}")
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- scrapers/relva.py
+++ scrapers/relva.py
@@ -1,9 +1,44 @@
 from bs4 import BeautifulSoup
 import requests
 from .base import ScraperStrategy
+import asyncio
+from playwright.async_api import async_playwright
+
+async def verificar_disponibilidade():
+    async with async_playwright() as p:
+        print("entrou")
+        browser = await p.chromium.launch(headless=True)  # headless=True significa sem abrir janela para debugar setar como FALSE
+        page = await browser.new_page()
+        await page.goto('https://relvaco.com.br/produto/camiseta-oversized-inferno-basica')
+        await page.wait_for_selector('.js-product-variants-group')
+   
+        #Nesse site tem um overlay então tem que fechar
+        close_layer = page.locator(".p-close")
+        if await close_layer.is_visible():
+            await close_layer.click()
+            await page.wait_for_timeout(1000)
+
+        # Espera o botão do tamanho ficar disponível
+        await page.locator("#product_form a[title='G']").first.wait_for(state="visible") #fazer entrada do tamanho por parametro depois
+        # Clica no botão G
+        await page.locator("#product_form a[title='G']").first.click()
+        
+        # Espera o botão atualizar após selecionar o tamanho
+        await page.wait_for_selector("input.js-addtocart", state="attached")
+
+        botao = await page.query_selector("input.js-addtocart")
+        value = await botao.get_attribute("value")
+        await browser.close()
+
+
+        disponibilidade = value.strip().lower()
+        if "comprar" in disponibilidade:
+            return("Produto disponível!")
+        else:
+            return(f"Indisponível: '{value}'")
 
 class RelvaScraper(ScraperStrategy):
-    def extrair_info(self, url: str) -> dict:
+    async def extrair_info(self, url: str) -> dict:
         headers = {'User-Agent': 'Mozilla/5.0'}
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -17,6 +52,6 @@ class RelvaScraper(ScraperStrategy):
         preco = preco_element.text.strip() if preco_element else 'Preço não encontrado'
         preco_original = preco_original.text.strip() if preco_original else 'Preço não encontrado'
 
-        estoque = 'ainda n funciona' if 'Esgotado' not in soup.text else 'Esgotado'
+        estoque = await verificar_disponibilidade()
 
         return {"nome": nome, "preco": preco, "preco original": preco_original,"estoque": estoque}
